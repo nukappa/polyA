3f7e14d5b6afcbfa9992e604c3a3f38dfbaa6f3e
nukappa <nukappa@users.noreply.github.com>
2016-04-15 22:27:54 +0200

    started writing discretize_bioanalyzer_profile

--- estimate_polyA_tail_length.py
+++ estimate_polyA_tail_length.py
@@ -3,6 +3,12 @@ import io
 def compute_distances_of_reads_to_all_pAi(bamfil):
     return 0
 
+def discretize_bioanalyzer_profile(profile, bin_size):
+    """Discretizes a given bioanalyzer profile by putting fragment
+       sizes into bins of given bin_size. The intensities are 
+       transformed into probabilities. A ????? is returned"""
+    return 0
+
 def assign_probabilities_to_pAi(distances, pAi):
     return 0
 
