da1c3c49e2ece445e5acc91442cbd1f178049975
nukappa <nukappa@users.noreply.github.com>
2016-04-18 12:53:33 +0200

    started unit_test + documentation

---
+++ unit_test.py
@@ -0,0 +1,21 @@
+import unittest
+import os
+from estimate_polyA_tail_length import *
+from extract_annotation import *
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_bioanalyzer_probabilities_summing_to_one(self):
+        bio_size = []
+        bio_intensity = []
+        
+        with open(os.path.join('test_data', 'ds_012_50fix_bioanalyzer.txt'), 'r') as f:
+            for line in f:
+                bio_size.append(int(line.split()[0]))
+                bio_intensity.append(float(line.split()[1]))
+
+        size, probability = discretize_bioanalyzer_profile(bio_size, bio_intensity, 5)
+        self.assertEqual(round(sum(probability), 12), 1)
+
+suite = unittest.TestLoader().loadTestsFromTestCase(TestStringMethods)
+unittest.TextTestRunner(verbosity=2).run(suite)
