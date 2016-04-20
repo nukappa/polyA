1abd0cbeaf0f10f0eac9f008523eafeb261fba8b
nukappa <nukappa@users.noreply.github.com>
2016-04-20 16:34:21 +0200

    major cleanup

--- estimate_polyA_tail_length.py
+++ estimate_polyA_tail_length.py
@@ -5,7 +5,7 @@
 # about #
 #########
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __author__ = ["Nikolaos Karaiskos","Marcel Schilling"]
 __credits__ = ["Nikolaos Karaiskos","Mireya Plass Pórtulas","Marcel Schilling","Nikolaus Rajewsky"]
 __status__ = "beta"
@@ -31,24 +31,6 @@ def discretize_bioanalyzer_profile(size, intensity, bin_size):
     """Discretizes a given bioanalyzer profile intensity=f(size) by putting 
        fragment sizes into bins of given bin_size. The intensities are 
        then transformed into probabilities."""
-    size = [round(s/bin_size)*bin_size for s in size]
-    probability = []
-
-    for bin in range(min(size), max(size)+1, bin_size):
-        indices = [i for i, x in enumerate(size) if x == bin]
-        total_bin_intensity = 0
-
-        for j in indices:
-            total_bin_intensity += intensity[j]
-
-        probability.append(total_bin_intensity/sum(intensity))
-
-    return list(sorted(set(size))), probability
-
-def discretize_bioanalyzer_profile_np(size, intensity, bin_size):
-    """Discretizes a given bioanalyzer profile intensity=f(size) by putting 
-       fragment sizes into bins of given bin_size. The intensities are 
-       then transformed into probabilities."""
     bins = np.arange(min(size), max(size)+1, bin_size)
     size = np.digitize(size, bins) * bin_size + min(size) - bin_size
     probability = np.array([sum(intensity[size == x])/sum(intensity) for x in np.unique(size)])
@@ -60,87 +42,33 @@ def step_function(x):
     return 1 * (x > 0)
 
 def tail_length_range(start, end, step):
-    return list(range(start, end, step))
-
-def tail_length_range_np(start, end, step):
     return np.arange(start, end, step)
 
 def prob_d_given_pAi(read_coordinate, pAi, interval, f, prob_f):
     """Computes the conditional probability P(d|pAi) that a read to
        originate from the particular pAi, given a bioanalyzer profile."""
-    nominator = 0
-    for fragment in range(len(f)):
-        nominator += (prob_f[fragment] * step_function(f[fragment] - (pAi[interval]['start']-read_coordinate)) * 
-                      step_function(pAi[interval]['end'] - read_coordinate - f[fragment]))
-
-    # normalization factor for sum(prob)=1
-    norm_factor = 0
-    for interval2 in range(len(pAi)):
-        temp_sum = 0
-        for fragment in range(len(f)):
-            temp_sum += (prob_f[fragment] * step_function(f[fragment] - (pAi[interval2]['start']-read_coordinate)) *
-                         step_function(pAi[interval2]['end'] - read_coordinate - f[fragment]))
-        norm_factor += temp_sum
-
-    return nominator/norm_factor
-
-def prob_d_given_pAi_np(read_coordinate, pAi, interval, f, prob_f):
-    """Computes the conditional probability P(d|pAi) that a read to
-       originate from the particular pAi, given a bioanalyzer profile."""
     nominator = sum(prob_f * step_function(f - pAi[interval]['start'] + read_coordinate) * 
                     step_function(pAi[interval]['end'] - read_coordinate - f))
-
     # normalization factor for sum(prob)=1
     norm_factor = sum([sum(prob_f * step_function(f - pAi[i]['start'] + read_coordinate) * 
                        step_function(pAi[i]['end'] - read_coordinate - f)) for i in range(len(pAi))])
-
     return nominator/norm_factor
-
 
 def prob_pAi_given_d(pAi, interval, read_coordinate, f, prob_f):
     """Computes the conditional probability P(pAi|d) for a pAi to give 
        rise to the read d. Prior probabilities for each pAi are taken to
        be homogeneous, namely 1/N, N=number of pAis."""
     nominator = prob_d_given_pAi(read_coordinate, pAi, interval, f, prob_f)
-    denominator = 0
-    for interval2 in range(len(pAi)):
-        denominator += prob_d_given_pAi(read_coordinate, pAi, interval2, f, prob_f)
-    return nominator/denominator
-
-def prob_pAi_given_d_np(pAi, interval, read_coordinate, f, prob_f):
-    """Computes the conditional probability P(pAi|d) for a pAi to give 
-       rise to the read d. Prior probabilities for each pAi are taken to
-       be homogeneous, namely 1/N, N=number of pAis."""
-    nominator = prob_d_given_pAi_np(read_coordinate, pAi, interval, f, prob_f)
-    denominator = sum([prob_d_given_pAi_np(read_coordinate, pAi, intrv, f, prob_f) for intrv in range(len(pAi))])
+    denominator = sum([prob_d_given_pAi(read_coordinate, pAi, intrv, f, prob_f) for intrv in range(len(pAi))])
     return nominator/denominator
 
 def prob_d_given_L(read_coordinate, pAi, interval, Length, f, prob_f, length_range):
     """Computes the conditional probability P(d|L) given the genomic coordinate
        of the read, a set of pAis, which of the pAis is the polyA tail, a length
        value, a bioanalyzer and a range for L."""
-    pAi[interval]['end'] = pAi[interval]['start'] + Length
-    nominator = 0
-    for fragment in range(len(f)):
-        nominator += prob_f[fragment] * 1/Length * step_function(pAi[interval]['end'] - read_coordinate - f[fragment])
-    
-    # compute the norm_factor for sum(prob)=1
-    norm_factor = 0
-    for length in length_range:
-        pAi[interval]['end'] = pAi[interval]['start'] + length
-        temp_sum = 0
-        for fragment in range(len(f)):
-            temp_sum += prob_f[fragment] * 1/length * step_function(pAi[interval]['end'] - read_coordinate - f[fragment])
-        norm_factor += temp_sum
-        
-    return nominator/norm_factor
-
-def prob_d_given_L_np(read_coordinate, pAi, interval, Length, f, prob_f, length_range):
-    """Computes the conditional probability P(d|L) given the genomic coordinate
-       of the read, a set of pAis, which of the pAis is the polyA tail, a length
-       value, a bioanalyzer and a range for L."""
     nominator = sum(prob_f * 1/Length * step_function(pAi[interval]['start'] + Length - read_coordinate - f))
-    norm_factor = sum([sum(prob_f * 1/length * step_function(pAi[interval]['start'] + length - read_coordinate - f)) for length in length_range])
+    norm_factor = sum([sum(prob_f * 1/length * 
+                       step_function(pAi[interval]['start'] + length - read_coordinate - f)) for length in length_range])
     return nominator/norm_factor
 
 def prob_d_given_L_weighted(read_coordinate, pAi, interval, Length, f, prob_f, length_range):
@@ -148,58 +76,18 @@ def prob_d_given_L_weighted(read_coordinate, pAi, interval, Length, f, prob_f, l
        of the read, a set of pAis, which of the pAis is the polyA tail, a length
        value, a bioanalyzer and a range for L."""
     pAi[interval]['end'] = pAi[interval]['start'] + Length
-    nominator = 0
-    for fragment in range(len(f)):
-        nominator += (prob_f[fragment] * 1/Length * 
-                      step_function(pAi[interval]['end'] - read_coordinate - f[fragment]) * 
-                      prob_d_given_pAi(read_coordinate, pAi, interval, f, prob_f))
-
-    # compute the norm_factor for sum(prob)=1
-    norm_factor = 0
-    for length in length_range:
-        pAi[interval]['end'] = pAi[interval]['start'] + length
-        temp_sum = 0 
-        for fragment in range(len(f)):
-            temp_sum += (prob_f[fragment] * 1/length * 
-                         step_function(pAi[interval]['end'] - read_coordinate - f[fragment]) * 
-                         prob_d_given_pAi(read_coordinate, pAi, interval, f, prob_f))
-        norm_factor += temp_sum
-
-    return nominator/norm_factor
-
-def prob_d_given_L_weighted_np(read_coordinate, pAi, interval, Length, f, prob_f, length_range):
-    """Computes the conditional probability P(d|L) given the genomic coordinate
-       of the read, a set of pAis, which of the pAis is the polyA tail, a length
-       value, a bioanalyzer and a range for L."""
-    pAi[interval]['end'] = pAi[interval]['start'] + Length
     nominator = sum(prob_f * 1/Length * step_function(pAi[interval]['end'] - read_coordinate - f) *
-                    prob_d_given_pAi_np(read_coordinate, pAi, interval, f, prob_f))
+                    prob_d_given_pAi(read_coordinate, pAi, interval, f, prob_f))
 
     # compute the norm_factor for sum(prob)=1
     norm_factor = 0
     for length in length_range:
         pAi[interval]['end'] = pAi[interval]['start'] + length
         norm_factor += sum(prob_f * 1/length * step_function(pAi[interval]['end'] - read_coordinate - f) * 
-                           prob_d_given_pAi_np(read_coordinate, pAi, interval, f, prob_f))
+                           prob_d_given_pAi(read_coordinate, pAi, interval, f, prob_f))
     return nominator/norm_factor
 
 def estimate_poly_tail_length(reads, tail_range, pAi, interval, f, prob_f, weighted):
-    """Takes a set of reads (list of read_coordinates), a range of polyA tail
-       lengths, a set of internal priming intervals and a bioanalyzer profile."""
-    L_probs = []
-    nominator = [1] * len(tail_range)
-    for read in reads:
-        read_probs = []
-        if weighted:
-            for L in tail_range:
-                read_probs.append(prob_d_given_L_weighted(read, pAi, interval, L, f, prob_f, tail_range))
-        else:
-            for L in tail_range:
-                read_probs.append(prob_d_given_L(read, pAi, interval, L, f, prob_f, tail_range))
-        nominator = [n*rp for n, rp in zip(nominator, read_probs)]
-    return nominator/sum(nominator)
-
-def estimate_poly_tail_length_np(reads, tail_range, pAi, interval, f, prob_f, weighted):
     """Takes a set of reads (list of read_coordinates), a range of polyA tail
        lengths, a set of internal priming intervals and a bioanalyzer profile.
        Homogeneous prior probabilities for the p(L) are assumed."""
@@ -209,9 +97,9 @@ def estimate_poly_tail_length_np(reads, tail_range, pAi, interval, f, prob_f, we
         read_probs = []
         for L in tail_range:
             if weighted:
-                read_probs.append(prob_d_given_L_weighted_np(read, pAi, interval, L, f, prob_f, tail_range))
+                read_probs.append(prob_d_given_L_weighted(read, pAi, interval, L, f, prob_f, tail_range))
             else:
-                read_probs.append(prob_d_given_L_np(read, pAi, interval, L, f, prob_f, tail_range))
+                read_probs.append(prob_d_given_L(read, pAi, interval, L, f, prob_f, tail_range))
         nominator *= read_probs
     return nominator/sum(nominator)
 
@@ -222,39 +110,4 @@ def estimate_poly_tail_length_np(reads, tail_range, pAi, interval, f, prob_f, we
 
 if __name__ == '__main__':
     start_time = time.time()
-    bio_size = []
-    bio_intensity = []
-    with open(os.path.join('test_data', 'ds_012_50fix_bioanalyzer.txt'), 'r') as f:
-        for line in f:
-            bio_size.append(int(line.split()[0]))
-            bio_intensity.append(float(line.split()[1]))
-
-    size, probability = discretize_bioanalyzer_profile(bio_size, bio_intensity, 5)
-
-    Lrange = tail_length_range(10, 250, 20)
-
-    pAi = [{'start' : 500, 'end' : 541, 'strand' : '+', 'is_tail' : False},
-           {'start' : 600, 'end' : 621, 'strand' : '+', 'is_tail' : False},
-           {'start' : 650, 'end' : 691, 'strand' : '+', 'is_tail' : True}]
-
-    print ('computing probabilities of read with distance 97 originating from polyA', 
-           'tail with length in range(10, 250, 20)')
-    for length in Lrange:
-        print ('for length', length, 'probability is', prob_d_given_L(650-97, pAi, 2, length, size, probability, Lrange))
-
-    print ('\n' + 'computing probabilities for observing read originating from pAi')
-
-    for interval in range(len(pAi)):
-        print ('for pAi', interval, pAi[interval]['is_tail'], 'being a polyA, probability is', 
-                prob_d_given_pAi(650-97, pAi, interval, size, probability))
-
-    print ('\n' + 'computing probabilities for pAi giving rise to read d')
-    for interval in range(len(pAi)):
-        print ('for pAi', interval, 'probability is', prob_pAi_given_d(pAi, interval, 650-897, size, probability))
-
-    print ('\n' + 'computing probabilities for polyA tail lengths')
-
-    reads = [650-897]
-    print (estimate_poly_tail_length(reads, Lrange, pAi, 2, size, probability, True)) 
-
     print ('\n',  time.time() - start_time, 'seconds elapsed')
--- unit_test.py
+++ unit_test.py
@@ -11,6 +11,8 @@ pAi = [{'start' : 500, 'end' : 541, 'strand' : '+', 'is_tail' : False},
        {'start' : 600, 'end' : 621, 'strand' : '+', 'is_tail' : False},                                                                                                                                                                                                               
        {'start' : 650, 'end' : 690, 'strand' : '+', 'is_tail' : True}]    
 
+reads = [550, 567, 568, 578, 579, 581, 600, 611]
+
 bio_size = []
 bio_intensity = []
 
@@ -19,58 +21,36 @@ with open(os.path.join('test_data', 'ds_012_50fix_bioanalyzer.txt'), 'r') as f:
         bio_size.append(int(line.split()[0]))
         bio_intensity.append(float(line.split()[1]))
 
-size, probability = discretize_bioanalyzer_profile(bio_size, bio_intensity, 5)
-size_np, probability_np = discretize_bioanalyzer_profile_np(np.array(bio_size), np.array(bio_intensity), 5)
+f_size, f_prob = discretize_bioanalyzer_profile(np.array(bio_size), np.array(bio_intensity), 5)
 
 Lrange = tail_length_range(10, 200, 25)
-Lrange_np = tail_length_range_np(10, 200, 25)
 
 class TestStringMethods(unittest.TestCase):
 
     def test_bioanalyzer_probabilities_summing_to_one(self):
-        self.assertEqual(round(sum(probability), PRECISION), 1)
+        self.assertEqual(round(sum(f_prob), PRECISION), 1)
 
     def test_prob_d_given_L_summing_to_one(self):
         Lrange = tail_length_range(10, 250, 5)
         prob_sum = 0
         for length in Lrange:
-            prob_sum += prob_d_given_L(650-97, pAi, 2, length, size, probability, Lrange)
+            prob_sum += prob_d_given_L(650-97, pAi, 2, length, f_size, f_prob, Lrange)
         self.assertEqual(round(prob_sum, PRECISION), 1)
 
     def test_prob_read_given_pAi_summing_to_one(self):
         prob_sum = 0
         for interval in range(len(pAi)):
-            prob_sum += prob_d_given_pAi(650-97, pAi, interval, size, probability)
+            prob_sum += prob_d_given_pAi(650-97, pAi, interval, f_size, f_prob)
         self.assertEqual(round(prob_sum, PRECISION), 1)
 
-    def test_bioanalyzer_probabilities_summing_to_one_np(self):
-        self.assertEqual(round(sum(probability_np), PRECISION), 1)
-
-    def test_prob_read_given_pAi_summing_to_one_np(self):
+    def test_prob_pAi_given_read_summing_to_one(self):
         prob_sum = 0
         for interval in range(len(pAi)):
-            prob_sum += prob_d_given_pAi_np(650-97, pAi, interval, size_np, probability_np)
+            prob_sum += prob_pAi_given_d(pAi, interval, 650-97, f_size, f_prob)
         self.assertEqual(round(prob_sum, PRECISION), 1)
 
-    def test_prob_pAi_given_read_summing_to_one_np(self):
-        prob_sum = 0
-        for interval in range(len(pAi)):
-            prob_sum += prob_pAi_given_d_np(pAi, interval, 650-97, size_np, probability_np)
-        self.assertEqual(round(prob_sum, PRECISION), 1)
-
-    def test_migration_to_np(self):
-        self.assertEqual(prob_d_given_pAi(650-97, pAi, 2, size_np, probability_np), 
-                         prob_d_given_pAi_np(650-97, pAi, 2, size_np, probability_np))
-        self.assertEqual(prob_pAi_given_d(pAi, 2, 650-97, size_np, probability_np),
-                         prob_pAi_given_d_np(pAi, 2, 650-97, size_np, probability_np))
-        self.assertEqual(prob_d_given_L(650-97, pAi, 2, 130, size_np, probability_np, Lrange),
-                         prob_d_given_L_np(650-97, pAi, 2, 130, size_np, probability_np, Lrange_np))
-        self.assertEqual(prob_d_given_L_weighted(650-97, pAi, 2, 130, size_np, probability_np, Lrange),
-                         prob_d_given_L_weighted_np(650-97, pAi, 2, 130, size_np, probability_np, Lrange_np))
-        self.assertEqual(estimate_poly_tail_length([5], Lrange, pAi, 2, size_np, probability_np, False),
-                         estimate_poly_tail_length_np([5], Lrange_np, pAi, 2, size_np, probability_np, False))
-        self.assertEqual(estimate_poly_tail_length([5], Lrange, pAi, 2, size_np, probability_np, True),
-                         estimate_poly_tail_length_np([5], Lrange_np, pAi, 2, size_np, probability_np, True))
+    def test_estimate_poly_tail_length_probs_summing_to_one(self):
+        self.assertEqual(sum(estimate_poly_tail_length(reads, Lrange, pAi, 2, f_size, f_prob, True)), 1) 
 
 suite = unittest.TestLoader().loadTestsFromTestCase(TestStringMethods)
 unittest.TextTestRunner(verbosity=2).run(suite)
