431e7f42eba24f9cb26fbc586b35db274b28cc59
Marcel Schilling <marcel.schilling@mdc-berlin.de>
2016-04-22 10:58:18 +0200

    implemented per-gene summary of 3'UTR isoforms reported (fixes #21) / cosmetics

--- extract_annotation.py
+++ extract_annotation.py
@@ -5,7 +5,7 @@
 # about #
 #########
 
-__version__ = "0.1.1.1"
+__version__ = "0.1.2"
 __author__ = ["Nikolaos Karaiskos","Marcel Schilling"]
 __credits__ = ["Nikolaos Karaiskos","Mireya Plass Pórtulas","Marcel Schilling","Nikolaus Rajewsky"]
 __status__ = "beta"
@@ -70,16 +70,29 @@ def extract_three_prime_utr_information(gtf_file,
     # The attribute with the following index will be used as gene ID:
     attribute_index_gene_id = 0
 
-    # Attribute type/value pairs will be split by the following character:
+    # Attribute type/value pairs will be split by the following
+    # character:
     attribute_separator = ' '
 
     # The following index will be used to get the value of an attribute
     # type/value pair:
     attribute_value_index = 1
 
-    # The following character will be removed from the beginning and end of
-    # attribute values:
+    # The following character will be removed from the beginning and end
+    # of attribute values:
     attribute_value_quote = '"'
+
+    # This variable will be used to keep track of the previous gene
+    # The GTF input has to be sorted by gene ID (not checked).
+    # This is necessary to be able to remove duplicate 3' UTRs arising
+    # from different isoforms sharing the same 3'UTR without the need to
+    # keep all 3' UTR BED entries in memory.
+    # Instead, only those for one gene at a time are stored.
+    previous_gene_id=None
+
+    # This set will be used to store all 3' UTR BED entries for the
+    # current gene (see above).
+    three_prime_utrs=set()
 
     # Read GTF input line by line
     with open_file(gtf_file) as gtf:
@@ -90,7 +103,8 @@ def extract_three_prime_utr_information(gtf_file,
                 continue
 
             # Split lines into fields
-            seqname, source, feature, start, end, score, strand, frame, attributes = line.rstrip().split(field_separator)
+            (seqname, source, feature, start, end, score, strand, frame,
+                    attributes) = line.rstrip().split(field_separator)
 
             # Skip lines not defining 3' UTRs
             if (feature != feature_utr3):
@@ -100,16 +114,37 @@ def extract_three_prime_utr_information(gtf_file,
             start = (int(start) - 1)
 
             # Get gene ID from attributes list
-            # I guess there is a more pythonic way to do that generating a
-            # dictionary but this works.
+            # I guess there is a more pythonic way to do that generating
+            # a dictionary but this works.
             # Split accross several lines for clarity.
             attributes = attributes.split(attributes_separator)
-            gene_id = attributes[attribute_index_gene_id].split(attribute_separator)
-            gene_id = gene_id[attribute_value_index].strip(attribute_value_quote)
+            gene_id = attributes[attribute_index_gene_id]
+            gene_id = gene_id.split(attribute_separator)
+            gene_id = gene_id[attribute_value_index]
+            gene_id = gene_id.strip(attribute_value_quote)
 
-            # output BED line
-            print("{seqname}\t{start}\t{end}\t{gene_id}\t{strand}\t{score}".format(**locals()))
-    return 0
+            # print BED line for each (different) 3' UTR isoform of the
+            # previous gene
+            if (gene_id!=previous_gene_id):
+                if (previous_gene_id is not None):
+                    for three_prime_utr in three_prime_utrs:
+                        print('\t'.join(str(field) for field in
+                                        three_prime_utr))
+
+            # re-initialize 3' UTR set for current gene
+                    three_prime_utrs=set()
+                previous_gene_id=gene_id
+
+            # append current 3' UTR to 3' UTRs of current gene (if not
+            # seen before)
+            three_prime_utrs.add((seqname,start,end,gene_id,strand,score))
+
+
+    # print BED line for each (different) 3' UTR isoform of the last
+    # gene
+    if (previous_gene_id is not None):
+        for three_prime_utr in three_prime_utrs:
+            print('\t'.join(str(field) for field in three_prime_utr))
 
 
 ########
