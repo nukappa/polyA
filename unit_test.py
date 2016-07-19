0fcd6c8a1ba830bc026be2fa01df505310045b90
Marcel Schilling <marcel.schilling@mdc-berlin.de>
2016-07-19 16:12:02 +0200

    increased number of reads simulated per gene to satisfy unit test with new fragment size distribution (closes #66)

--- unit_test.py
+++ unit_test.py
@@ -61,7 +61,7 @@ gtf_url = 'ftp://ftp.ensembl.org/pub/release-84/gtf/homo_sapiens/Homo_sapiens.GR
 gtf = os.path.join(folder_in, 'Homo_sapiens.GRCh38.84_chr9.gtf.gz')
 
 num_genes_sim = 5
-reads_per_gene = 1500
+reads_per_gene = 3000
 pAlen_sim = 42
 offset_min = 1
 
